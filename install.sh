80c2f7adefd9af1ef79ea1924591bd511d9d1824
Oliver Eisenhut <oliver.eisenhut@gmail.com>
2017-03-23 12:31:59 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 SCRIPTPATH=`pwd -P`
 
@@ -14,14 +14,14 @@ wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key ad
 # -> yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 
-#add repositories
-#jdk
+# add repositories
+# jdk
 sudo add-apt-repository ppa:openjdk-r/ppa
-#spotify
+# spotify
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-#chrome
+# chrome
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-#yarn
+# yarn
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 
@@ -36,53 +36,16 @@ sudo apt-get upgrade -y
 
 
 ######################
-# Management Software
+# PACKAGES
 ######################
 
-#aptitude
-sudo apt-get install aptitude -y
-
-#vim
-sudo apt-get install vim -y
-
-#zsh
-sudo apt-get install zsh -y
-
-#tmux
-sudo apt-get install tmux -y
-
-#htop
-sudo apt-get install htop -y
-
-#openjdk 8
-sudo apt-get install openjdk-8-jdk -y
-
-#imagemagick
-sudo apt-get install imagemagick -y
-
-#curl
-sudo apt-get intall curl -y
-
-#git
-sudo apt-get install git -y
-
-#google chrome
-sudo apt-get install google-chrome-stable -y
-
-#firefox
-sudo apt-get install firefox -y
-
-#spotify
-sudo apt-get install spotify-client -y
-
-#yarn
-sudo apt-get install yarn -y
+sudo apt install aptitude  vim zsh tmux htop openjdk-8-jdk imagemagick culr git google-chrome-stable firefox spotify-client yarn -y
 
 ######################
 # Zsh
 ######################
 
-#oh-my-zsh
+# oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # spaceship theme
@@ -94,47 +57,34 @@ curl -o - https://raw.githubusercontent.com/denysdovhan/spaceship-zsh-theme/mast
 
 # NODE JS #
 
-#nvm => https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-16-04
-curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
+# nvm => https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-16-04
+curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh -o install_nvm.sh
 sudo -u "$(whoami)" bash install_nvm.sh
 source ~/.bashrc
+source ~/.bash_profile
+source ~/.zshrc
 
 #install lts verison
-nvm install 4.4.7
-nvm use 4.4.7
+nvm install stable
+nvm use stable
 
 # RUBY #
 
-#rvm => https://rvm.io/rvm/install
-curl -sSL https://get.rvm.io | bash -s stable --ruby
+# rvm => https://rvm.io/rvm/install
+curl -sSL https://get.rvm.io | bash -s stable --with-default-gems="rails bundler" --ruby
 source ~/.bashrc
-rvm install ruby-2.4.0
+
 
 # Editors and Ides #
 
-#Atom
-curl -s -L https://atom.io/download/deb -o atom_installer.deb
-sudo dpkg -i atom_installer.deb
-sudo rm -f atom_installer
+# Jetbrains Toolbox
 
+curl https://download-cf.jetbrains.com/toolbox/jetbrains-toolbox-1.2.2314.tar.gz | tar xvz
+cd jetbrains-toolbox-1.2.2314
+./jetbrains-toolbox
+cd SCRIPTPATH
 
-#Phpstorm
-curl -L https://download-cf.jetbrains.com/webide/PhpStorm-2016.3.2.tar.gz -o phpstorm_installer.tar.gz
-sudo mv phpstorm_installer.tar.gz /opt
-cd /opt
-sudo tar xfz phpstorm_installer.tar.gz
-sudo rm -f phpstorm_installer.tar.gz
-cd ~/Downloads
-
-#IntelliJ community
-curl -L https://download-cf.jetbrains.com/idea/ideaIU-2016.3.1-no-jdk.tar.gz -o intelliJ.tar.gz
-sudo mv intelliJ.tar.gz /opt
-cd /opt
-sudo tar xfz intelliJ.tar.gz
-sudo rm -f intelliJ.tar.gz
-cd ~/Downloads
-
-#Vim
+# Vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 source ~/.profile
@@ -146,25 +96,9 @@ function install_elementary_stuff() {
   wget -O xt  http://git.io/v6JYg && chmod +x xt && ./xt && rm xt
 }
 
-function install_atom_packages() {
-  #beautify
-  apm install atom-beautify
-  apm install pigments
-  apm install react
-
-  #themes
-  apm install gruvbox-plus-syntax
-  apm install octocat-syntax
-
-  #linters & co
-  apm install linter
-  apm install linter-eslint
-  apm install linter-stylelint
-
-}
-
 function install_npm_packages() {
-   yarn global add gulp grunt gatsby create-react-app 
+   npm install -g gulp grunt create-react-app diff-so-fancy
+   git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 }
 
 function copy_dotfiles() {
@@ -173,7 +107,6 @@ function copy_dotfiles() {
 }
 
 #subscripts
-install_atom_packages
 install_elementary_stuff
 copy_dotfiles
 install_npm_packages
