270c87e6c7073645f523cde66499f40f1df37f4e
Oliver Eisenhut <oliver.eisenhut@frontify.com>
2024-06-23 19:37:04 +0200

    Add custom snippets

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -75,6 +75,7 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 -- this is the function that loads the extra snippets to luasnip
 -- from rafamadriz/friendly-snippets
+require("luasnip.loaders.from_vscode").load({ paths = "./my_snippets" })
 require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
--- nvim/.config/nvim/lua/moi/lazy.lua
+++ nvim/.config/nvim/lua/moi/lazy.lua
@@ -46,10 +46,10 @@ require("lazy").setup({
 			{ "hrsh7th/cmp-nvim-lsp" },
 			{ "hrsh7th/cmp-buffer" },
 			{ "hrsh7th/cmp-path" },
-			{ "saadparwaiz1/cmp_luasnip" },
 			{ "hrsh7th/cmp-nvim-lua" },
 
 			-- Snippets
+			{ "saadparwaiz1/cmp_luasnip" },
 			{ "L3MON4D3/LuaSnip" },
 			{ "rafamadriz/friendly-snippets" },
 		},
