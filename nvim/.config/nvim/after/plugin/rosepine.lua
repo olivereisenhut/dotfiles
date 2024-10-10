e1644e5fbc65472bbda552625b4cffa12f845f45
Oliver Eisenhut <oliver.eisenhut@frontify.com>
2024-10-10 18:32:48 +0200

    Always use main rosepine theme

--- nvim/.config/nvim/after/plugin/rosepine.lua
+++ nvim/.config/nvim/after/plugin/rosepine.lua
@@ -1,6 +1,6 @@
 require("rose-pine").setup({
 	variant = "main", -- auto, main, moon, or dawn
-	dark_variant = "moon", -- main, moon, or dawn
+	dark_variant = "main", -- main, moon, or dawn
 })
 
 vim.cmd("colorscheme rose-pine")
