f3216a043deb7262dd46aca0618ded8bb36900e7
Oliver Eisenhut <oliver.eisenhut@frontify.com>
2024-11-07 22:50:38 +0100

    Disable oil preview keymap

--- nvim/.config/nvim/after/plugin/oil.lua
+++ nvim/.config/nvim/after/plugin/oil.lua
@@ -2,6 +2,9 @@ require("oil").setup({
 	view_options = {
 		show_hidden = true,
 	},
+	keymaps = {
+		["<C-p>"] = false,
+	},
 })
 
 vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
