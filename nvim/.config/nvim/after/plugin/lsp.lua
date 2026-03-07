ba1fb44886610a00928ca871695e2182d3d28753
Oliver Eisenhut <oliver.eisenhut@frontify.com>
2026-03-07 23:06:31 +0100

    Migrate to new way of registering lsp configs

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -38,33 +38,32 @@ local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 require("mason").setup({})
 require("mason-lspconfig").setup({
 	ensure_installed = { "ts_ls", "rust_analyzer" },
-	handlers = {
-		function(server_name)
-			require("lspconfig")[server_name].setup({
-				capabilities = lsp_capabilities,
-			})
-		end,
-		lua_ls = function()
-			require("lspconfig").lua_ls.setup({
-				capabilities = lsp_capabilities,
-				settings = {
-					Lua = {
-						runtime = {
-							version = "LuaJIT",
-						},
-						diagnostics = {
-							globals = { "vim" },
-						},
-						workspace = {
-							library = {
-								vim.env.VIMRUNTIME,
-							},
-						},
-					},
+})
+
+vim.lsp.config("*", {
+	capabilities = lsp_capabilities,
+})
+
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = {
+					vim.env.VIMRUNTIME,
 				},
-			})
-		end,
+			},
+		},
 	},
+})
+
+vim.lsp.config("expert", {
+	cmd = { "expert", "--stdio" },
 })
 
 local cmp = require("cmp")
